f63bf42cc2cd071a3fb27d7c33829663025d8f56
Voloshin <i8an89@mail.ru>
2025-10-04 13:55:38 +0300

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
 // отдаём класс конфигурации
-        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        final var context = new AnnotationConfigApplicationContext("org.example");
         final var controller = context.getBean("postController");
         final var service = context.getBean(PostService.class);
 
